648aff7f170464798778c87509dcc4d6a69a42be
AjayHebburRavindra <iamajayhr@gmail.com>
2023-11-13 16:19:05 +0100

    Your commit message

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dsss_homework_2',
+    name='your-package-name',
     version='0.1',
     packages=find_packages(),
     install_requires=[
