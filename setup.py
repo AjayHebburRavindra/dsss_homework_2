e498da949d4bb8fb99efdf6f900c1943beeb434c
AjayHebburRavindra <126474717+AjayHebburRavindra@users.noreply.github.com>
2023-11-13 21:19:29 +0100

    Description about setuptools is added.

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
-# Setup Git on your Windows Computer:
-
-# For Windows:
-
-# 1. Download Git:
-
-# a. Visit the official Git website: https://git-scm.com/downloads
-# b. The website will detect your operating system. Click on the Windows download link.
-
-# 2. Run the Installer:
-
-# a.Once the download is complete, run the installer
-# b. You'll go through several installation steps. You can generally choose the default settings, but you might want to make sure that "Git Bash" is selected to install, as it provides a Unix-like command-line environment on Windows.
-
-# 3. Adjust System PATH (if prompted):
-
-# a. During the installation, there might be an option to "Adjusting your PATH environment" or "Use Git from the Windows Command Prompt." Choose this option to add Git to your PATH so that you can use Git from the command line.
-
-# 4. Finish Installation:
-
-# a. Complete the installation by following the on-screen instructions.
-
-# 5. Verify Installation:
-
-# a. Open a new command prompt window and type git --version. If Git has been installed successfully, it will display the installed version.
+from setuptools import setup, find_packages
+setup(
+    name='Python_sample',
+    version='0.1',
+    packages=find_packages(),
+    py_modules=['math_quiz'],
+    install_requires=[ ],
+    entry_points={
+        'console_scripts': [],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    url='https://github.com/your-username/your-repo',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
